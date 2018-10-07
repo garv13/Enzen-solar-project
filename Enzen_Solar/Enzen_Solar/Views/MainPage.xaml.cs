d8651d16f3fedd2d1a66478c61e5e83df46d0f70
Garv Jasuja <garv@selfdrvn.com>
2018-10-07 23:36:01 +0530

    Dashboard UI

--- Enzen_Solar/Enzen_Solar/Views/MainPage.xaml.cs
+++ Enzen_Solar/Enzen_Solar/Views/MainPage.xaml.cs
@@ -7,11 +7,49 @@ using Xamarin.Forms;
 
 namespace Enzen_Solar.Views
 {
-	public partial class MainPage : TabbedPage
+    class DashboardDataView
+    {
+        public string WalletBalance { get; set; }
+
+        public string CoinBalance { get; set; }
+
+        public string ShareBalance { get; set; }
+    }
+	public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+            DashboardDataView db = new DashboardDataView();
+            db.CoinBalance = "18";
+            db.ShareBalance = "6";
+            db.WalletBalance = "22.5";
+            MainGrid.BindingContext = db;
 		}
-	}
+
+        private void AddWalletButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WithdrawWalletButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PayBillButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TradeCoinButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TradeShareButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
