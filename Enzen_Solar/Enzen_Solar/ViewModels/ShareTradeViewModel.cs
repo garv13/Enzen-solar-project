5fff9ea74c0eb5bcf6c4aedbba32a8b21808fde0
garv13 <garv13@gmail.com>
2018-10-21 02:05:20 +0530

    Share Trade Working

--- Enzen_Solar/Enzen_Solar/ViewModels/ShareTradeViewModel.cs
+++ Enzen_Solar/Enzen_Solar/ViewModels/ShareTradeViewModel.cs
@@ -23,6 +23,8 @@ namespace Enzen_Solar.ViewModels
 
         public string QuantityPurchase { get; set; }
 
+        public int RoofID { get; set; }
+
         public ICommand BuyCommand { get; set; }
     }
 
@@ -66,9 +68,11 @@ namespace Enzen_Solar.ViewModels
 
         private MobileServiceCollection<Share, Share> UserShareList;
         private MobileServiceCollection<Share, Share> SecondaryShareList;
+        private MobileServiceCollection<Roof, Roof> RoofList;
 
         IMobileServiceTable<Share> userShareTable;
         IMobileServiceTable<UserCredit> userCreditTable;
+        IMobileServiceTable<Roof> roofTable;
 
         public ICommand MyShareSellCommand { get; private set; }
         public ICommand NewShareBuyCommand { get; set; }
@@ -83,8 +87,12 @@ namespace Enzen_Solar.ViewModels
             MyShareSellCommand = new Command<MyShareListViewModel>(handleMyShareSellCommand);
             NewShareBuyCommand = new Command<NewShareListViewModel>(handleNewShareBuyCommand);
             SecondaryShareBuyCommand = new Command<SecondaryShareListViewModel>(handleSecondaryShareBuyCommand);
+
             userShareTable = App.MobileService.GetTable<Share>();
             userCreditTable = App.MobileService.GetTable<UserCredit>();
+            roofTable = App.MobileService.GetTable<Roof>();
+
+            PopulateNewShares();
             GetUserShares();
             PopulateSecondaryMarket();
         }
@@ -116,7 +124,7 @@ namespace Enzen_Solar.ViewModels
             {
                 if (SecondaryShareList != null)
                     SecondaryShareList.Clear();
-                SecondaryShareList = await userShareTable.Where(Share => Share.IsTradeable == false).ToCollectionAsync();
+                SecondaryShareList = await userShareTable.Where(Share => Share.IsTradeable == true).ToCollectionAsync();
                 var distinctList = SecondaryShareList.ToLookup(x => x.Price);
                 foreach(var share in distinctList)
                 {
@@ -128,52 +136,110 @@ namespace Enzen_Solar.ViewModels
             }
             catch(Exception) { }
         }
+
+        private async void PopulateNewShares()
+        {
+            try
+            {
+                if (RoofList != null)
+                    RoofList.Clear();
+                RoofList = await roofTable.Where(roof => roof.SharesAvailable > 0).ToCollectionAsync();
+                if (RoofList.Count != 0)
+                {
+                    // group by year
+                    foreach(Roof rf in RoofList)
+                    {
+                        NewShareListViewModel tp = new NewShareListViewModel();
+                        tp.ListQuantity = rf.SharesAvailable.ToString();
+                        tp.ListRate = rf.CostPerShare.ToString();
+                        tp.BuyCommand = NewShareBuyCommand;
+                        tp.RoofID = rf.RoofId;
+                        NewShareListViewObj.Add(tp);
+                    }                   
+                }
+            }
+
+            catch (Exception) { }
+        }
         private async void handleMyShareSellCommand(MyShareListViewModel item)
         {
-            var price = int.Parse(item.PurchaseRate);
-            var qty = int.Parse(item.QuantityPurchase);
-            for(int i = 0; i < qty; i++)
+            try
             {
-                UserShareList[i].IsTradeable = true;
-                UserShareList[i].Price = price;
-                await App.MobileService.GetTable<Share>().UpdateAsync(UserShareList[i]);              
+                var price = int.Parse(item.PurchaseRate);
+                var qty = int.Parse(item.QuantityPurchase);
+                for (int i = 0; i < qty; i++)
+                {
+                    UserShareList[i].IsTradeable = true;
+                    UserShareList[i].Price = price;
+                    await App.MobileService.GetTable<Share>().UpdateAsync(UserShareList[i]);
+                }
             }
+
+            catch (Exception) { }            
         }
 
 
         private async void handleNewShareBuyCommand(NewShareListViewModel item)
         {
-            var e = item.ListQuantity;
-            var w = item.ListRate;
+            try
+            {
+                int price = int.Parse(item.ListRate);
+                int qty = int.Parse(item.QuantityPurchase);
+
+                int cost = price * qty;
+
+                var Roof = await roofTable.Where(roof => roof.RoofId == item.RoofID).ToCollectionAsync();
+                if (Roof.Count != 0)
+                {
+                    Roof[0].SharesAvailable -= qty;
+                    await roofTable.UpdateAsync(Roof[0]);
+                }
+
+                var Buyer = await userCreditTable.Where(x => x.UserId == App.UserID).ToCollectionAsync();
+                Buyer[0].WalletBalance = (int.Parse(Buyer[0].WalletBalance) - cost).ToString();
+                await userCreditTable.UpdateAsync(Buyer[0]);
+
+                var AdminUser = await userCreditTable.Where(x => x.UserId == App.AdminUserID).ToCollectionAsync();
+                AdminUser[0].WalletBalance = (int.Parse(AdminUser[0].WalletBalance) + cost).ToString();
+                await userCreditTable.UpdateAsync(AdminUser[0]);
+
+            }
+
+            catch (Exception) { }
         }
 
 
         private async void handleSecondaryShareBuyCommand(SecondaryShareListViewModel item)
         {
-            int price = int.Parse(item.ListRate);
-            int qty = int.Parse(item.QuantityPurchase);
-
-            int cost = price * qty;
-            List<Share> AvailableShares = SecondaryShareList.Where(x => x.Price == price).ToList();
-            Dictionary<int, int> UserWalletUpdate = new Dictionary<int, int>();
-            for(int i= 0; i < qty; i++)
+            try
             {
-                AvailableShares[i].IsTradeable = true;
-                UserWalletUpdate[AvailableShares[i].UserId] = UserWalletUpdate[AvailableShares[i].UserId] + 1;
-                await userShareTable.UpdateAsync(AvailableShares[i]);
+                int price = int.Parse(item.ListRate);
+                int qty = int.Parse(item.QuantityPurchase);
+
+                int cost = price * qty;
+                List<Share> AvailableShares = SecondaryShareList.Where(x => x.Price == price).ToList();
+                Dictionary<int, int> UserWalletUpdate = new Dictionary<int, int>();
+                for (int i = 0; i < qty; i++)
+                {
+                    AvailableShares[i].IsTradeable = false;
+                    UserWalletUpdate[AvailableShares[i].UserId] = UserWalletUpdate[AvailableShares[i].UserId] + 1;
+                    AvailableShares[i].UserId = App.UserID;
+                    await userShareTable.UpdateAsync(AvailableShares[i]);
+                }
+
+                var Buyer = await userCreditTable.Where(x => x.UserId == App.UserID).ToCollectionAsync();
+                Buyer[0].WalletBalance = (int.Parse(Buyer[0].WalletBalance) - cost).ToString();
+                await userCreditTable.UpdateAsync(Buyer[0]);
+
+                foreach (var a in UserWalletUpdate)
+                {
+                    var Seller = await userCreditTable.Where(x => x.UserId == a.Key).ToCollectionAsync();
+                    Seller[0].WalletBalance = (int.Parse(Seller[0].WalletBalance) + cost * a.Value).ToString();
+                    await userCreditTable.UpdateAsync(Seller[0]);
+                }
             }
 
-            var Buyer = await userCreditTable.Where(x => x.UserId == App.UserID).ToCollectionAsync();
-            Buyer[0].WalletBalance = (int.Parse(Buyer[0].WalletBalance) - cost).ToString();
-            await userCreditTable.UpdateAsync(Buyer[0]);
-
-            foreach(var a in UserWalletUpdate)
-            {
-                var Seller = await userCreditTable.Where(x => x.UserId == a.Key).ToCollectionAsync();
-                Seller[0].WalletBalance = (int.Parse(Seller[0].WalletBalance) + cost * a.Value).ToString();
-                await userCreditTable.UpdateAsync(Seller[0]);
-            }
-
+            catch (Exception) { }            
         }
 
     }
